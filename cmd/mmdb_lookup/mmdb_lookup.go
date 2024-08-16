44c446734d7a53c4d2d384bea7f6846314a11b4c
vph <vph.persson@gmail.com>
2024-08-16 18:19:23 +0200

    Initial.

---
+++ cmd/mmdb_lookup/mmdb_lookup.go
@@ -0,0 +1,99 @@
+package main
+
+import (
+	"bufio"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"github.com/Motmedel/utils_go/utils_go"
+	"github.com/maxmind/mmdbinspect/pkg/mmdbinspect"
+	"io"
+	"log/slog"
+	"mmdb_lookup/pkg/mmdb_lookup"
+	"os"
+	"strings"
+	"sync"
+)
+
+func main() {
+	logger := slog.Default()
+
+	var databaseFilePath string
+	flag.StringVar(&databaseFilePath, "db", "", "A path of the database file.")
+
+	var ipAddressesFilePath string
+	flag.StringVar(&ipAddressesFilePath, "f", "-", "A path of a file of IP addresses.")
+
+	flag.Parse()
+
+	if databaseFilePath == "" {
+		fmt.Fprintln(os.Stderr, "error: -db is required\n")
+		fmt.Fprintf(os.Stderr, "Usage of %s:\n", os.Args[0])
+		flag.PrintDefaults()
+		os.Exit(1)
+		return
+	}
+
+	var ipAddressesInput io.Reader
+
+	if ipAddressesFilePath == "-" {
+		ipAddressesInput = os.Stdin
+	} else {
+		var err error
+		ipAddressesInput, err = os.Open(ipAddressesFilePath)
+		if err != nil {
+			utils_go.LogFatal("An error occurred when opening the IP addresses file.", err, logger, 1)
+		}
+		defer ipAddressesInput.(*os.File).Close()
+	}
+
+	databaseReader, err := mmdbinspect.OpenDB(databaseFilePath)
+	if err != nil {
+		utils_go.LogFatal("An error occurred when opening the mmdb database file.", err, logger, 1)
+	}
+
+	var printMutex sync.Mutex
+
+	scanner := bufio.NewScanner(ipAddressesInput)
+
+	mmdb_lookup.LookupNetworkIterator(
+		func(yield func(string) bool) {
+			for scanner.Scan() {
+				ipAddressString := scanner.Text()
+				if ipAddressString == "" {
+					continue
+				}
+
+				if !yield(ipAddressString) {
+					break
+				}
+			}
+		},
+		databaseReader,
+		logger,
+		func(records []*mmdbinspect.RecordForNetwork) {
+			for _, record := range records {
+				recordMap, ok := record.Record.(map[string]any)
+				if !ok {
+					utils_go.LogFatal(
+						"recordForNetwork.Record could not be converted to a map[string]any.",
+						nil,
+						logger,
+						1,
+					)
+				}
+				ipAddressString, _ := strings.CutSuffix(record.Network, "/32")
+				recordMap["ip_address"] = ipAddressString
+
+				data, err := json.Marshal(recordMap)
+				if err != nil {
+					utils_go.LogError("An error occurred when marshalling a record map.", err, logger)
+					continue
+				}
+				printMutex.Lock()
+				fmt.Println(string(data))
+				printMutex.Unlock()
+			}
+		},
+	)
+}
---
+++ pkg/mmdb_lookup/mmdb_lookup.go
@@ -0,0 +1,97 @@
+package mmdb_lookup
+
+import (
+	"errors"
+	"github.com/Motmedel/utils_go/utils_go"
+	"github.com/maxmind/mmdbinspect/pkg/mmdbinspect"
+	"github.com/oschwald/maxminddb-golang"
+	"iter"
+	"log/slog"
+	"slices"
+	"sync"
+)
+
+func Lookup(maybeNetwork string, reader *maxminddb.Reader) ([]*mmdbinspect.RecordForNetwork, error) {
+	recordsAny, err := mmdbinspect.RecordsForNetwork(*reader, true, maybeNetwork)
+	if err != nil {
+		return nil, &utils_go.InputError{
+			Message: "An error occurred when obtaining records.",
+			Cause:   err,
+			Input:   maybeNetwork,
+		}
+	}
+
+	records, ok := recordsAny.([]any)
+	if !ok {
+		return nil, errors.New("recordsAny could not be converted into a slice of any")
+	}
+
+	var recordForNetworkSlice []*mmdbinspect.RecordForNetwork
+
+	for _, record := range records {
+		recordForNetwork, ok := record.(mmdbinspect.RecordForNetwork)
+		if !ok {
+			return nil, errors.New("a record could not be converted into a RecordForNetwork")
+		}
+		recordForNetworkSlice = append(recordForNetworkSlice, &recordForNetwork)
+	}
+
+	return recordForNetworkSlice, nil
+}
+
+func LookupNetworkIterator(
+	seq iter.Seq[string],
+	reader *maxminddb.Reader,
+	logger *slog.Logger,
+	callback func([]*mmdbinspect.RecordForNetwork),
+) []*mmdbinspect.RecordForNetwork {
+	var allRecords []*mmdbinspect.RecordForNetwork
+
+	var waitGroup sync.WaitGroup
+	var recordsMutex sync.Mutex
+
+	for maybeNetwork := range seq {
+		if maybeNetwork == "" {
+			continue
+		}
+
+		maybeNetwork := maybeNetwork
+
+		waitGroup.Add(1)
+		go func() {
+			defer waitGroup.Done()
+
+			records, err := Lookup(maybeNetwork, reader)
+			if err != nil {
+				utils_go.LogError("An error occurred when retrieving records.", err, logger)
+			}
+
+			if len(records) != 0 {
+				if callback != nil {
+					callback(records)
+				}
+				recordsMutex.Lock()
+				allRecords = append(allRecords, records...)
+				recordsMutex.Unlock()
+			}
+		}()
+	}
+
+	waitGroup.Wait()
+
+	return allRecords
+}
+
+func LookupNetworks(
+	networks []string,
+	reader *maxminddb.Reader,
+	logger *slog.Logger,
+	callback func([]*mmdbinspect.RecordForNetwork),
+) []*mmdbinspect.RecordForNetwork {
+	return LookupNetworkIterator(
+		slices.Values(networks),
+		reader,
+		logger,
+		callback,
+	)
+}
